bea0af063574164b74613c8cd60c94bd3e0a31b0
Bye-Shuvo <abuhasnatshuvo605@gmail.com>
2026-02-23 00:39:04 +0600

    Added main logic for word ommitter component

--- src/components/WordOmitter.jsx
+++ src/components/WordOmitter.jsx
@@ -1,11 +1,53 @@
-import React from 'react'
+import React, { useState } from "react";
 
-const WordOmitter = () => {
+const OMITTED_WORDS = ["a", "the", "and", "or", "but"];
+
+function WordOmitter() {
+  const [inputText, setInputText] = useState("");
+  const [omitWords, setOmitWords] = useState(true);
+
+  const handleInputChange = (e) => {
+    setInputText(e.target.value);
+  };
+
+  const toggleOmitWords = () => {
+    setOmitWords(!omitWords);
+  };
+
+  const clearFields = () => {
+    // TODO: Add your changes here
+    setInputText('');
+  };
+
+  const getProcessedText = () => {
+    // TODO: Add your changes here
+      const omittedText = inputText.split(" ").filter((Word) => !OMITTED_WORDS.includes(Word)).join(" ");
+    return omittedText ;
+  };
+
   return (
-    <div>
-      
+    <div className="omitter-wrapper">
+      <textarea
+        placeholder="Type here..."
+        value={inputText}
+        onChange={handleInputChange}
+        data-testid="input-area"
+      />
+      <div>
+        <button onClick={toggleOmitWords} data-testid="action-btn">
+          {omitWords ? "Show All Words" : "Omit Words"}
+        </button>
+        <button onClick={clearFields} data-testid="clear-btn">
+          Clear
+        </button>
+      </div>
+      <div>
+        <h2>Output:</h2>
+        <p data-testid="output-text">{omitWords ? getProcessedText() : inputText}</p>
+      </div>
     </div>
-  )
+  );
 }
 
-export default WordOmitter
+export { WordOmitter };
+
